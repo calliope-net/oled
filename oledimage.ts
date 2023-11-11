6225c1000f5c09a0ca51798c0378a7e99a31b7b5
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-11-11 21:29:37 +0100

    Aktualisiere oledimage.ts

--- oledimage.ts
+++ oledimage.ts
@@ -9,44 +9,33 @@ neu programmiert von Lutz Elßner im November 2023
 */ {
 
 
-    //% block="Bild16x16"
-    //% imageLiteral=1
-    //% imageLiteralColumns=16
-    //% imageLiteralRows=16
-    //% shim=images::createImage
-    //% weight=50
     //% subcategory=paint
-    export function matrix16x16(i: string): Image {
-        const im = <Image><any>i;
-        return im
-    }
-
-    //% block="Bild8x8"
-    //% imageLiteral=1
-    //% imageLiteralColumns=8
-    //% imageLiteralRows=8
+    //% block="Bild8x8" weight=4
+    //% imageLiteral=1 imageLiteralColumns=8 imageLiteralRows=8
     //% shim=images::createImage
-    //% weight=55
-    //% subcategory=paint
     export function matrix8x8(i: string): Image {
         const im = <Image><any>i;
         return im
     }
+
+    //% subcategory=paint
+    //% block="Bild16x16" weight=2
+    //% imageLiteral=1 imageLiteralColumns=16 imageLiteralRows=16
+    //% shim=images::createImage
+    export function matrix16x16(i: string): Image {
+        const im = <Image><any>i;
+        return im
+    }
      
-    //% imageLiteral=1
-    //% imageLiteralColumns=6
-    //% imageLiteralRows=24
+    //% imageLiteral=1 imageLiteralColumns=6 imageLiteralRows=24
     //% shim=images::createImage
     export function matrix6x24(i: string): Image {
         const im = <Image><any>i;
         return im
     }
 
-    //% imageLiteral=1
-    //% imageLiteralColumns=22
-    //% imageLiteralRows=6
+    //% imageLiteral=1 imageLiteralColumns=22 imageLiteralRows=6
     //% shim=images::createImage
-    //% weight=90
     export function matrix22x6(i: string): Image {
         const im = <Image><any>i;
         return im
