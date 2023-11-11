9f9205602bc2b0893d2ec33e4ce5d7ef8b79738d
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-11-11 15:37:32 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, oledarrays.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
-})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    OLED16x8.writeText16x8(0, 0, 15, oledssd1315.oledssd1315_text("QWER"))
+    OLED16x8.writeText16x8(0, 0, 15, oledssd1315.oledssd1315_text(""))
 })
 let OLED16x8: oledssd1315.oledclass = null
 basic.showIcon(IconNames.Meh)
-OLED16x8 = oledssd1315.beimStart(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8_x3C), false, false, false)
+OLED16x8 = oledssd1315.beimStart(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8_x3D))
